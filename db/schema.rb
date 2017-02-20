a3d62d3df6d059e47419cfa50a104fe0803aee0a
sakai <sakai.417.tblbj@gmail.com>
2017-02-20 13:29:27 +0900

    add comment scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220042522) do
+ActiveRecord::Schema.define(version: 20170220042802) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "name"
+    t.string   "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.string   "name"
